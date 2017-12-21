5d43aa63074eca4dba297d84e6b3fb05eb105887
Josh Eigen <josh.eigen@gmail.com>
2017-12-21 13:54:50 -0700

    Implemented interfaces for all classes

--- Project/Game.cs
+++ Project/Game.cs
@@ -4,6 +4,22 @@ namespace CastleGrimtol.Project
 {
     public class Game : IGame
     {
+        public Room CurrentRoom { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public Player CurrentPlayer { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Setup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UseItem(string itemName)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Project/Room.cs
+++ Project/Room.cs
@@ -4,6 +4,13 @@ namespace CastleGrimtol.Project
 {
     public class Room : IRoom
     {
+        public string Name { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string Description { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public List<Item> Items { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
+        public void UseItem(Item item)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
